8563c1621eafdb2bd1e01b40b180025c9825a395
alphasow <alphaibrahima.sow@outlook.fr>
2024-11-02 13:25:30 +0100

    feat(auth): user form db

--- app/data/user_repository.py
+++ app/data/user_repository.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 
+from app.data.database import user_collection
+
 
 class User(BaseModel):
     id: str
@@ -9,3 +11,8 @@ class User(BaseModel):
     hashed_password: str
     disabled: bool | None = None
 
+
+class UserRepository:
+    @staticmethod
+    async def get_user_by_username(username: str):
+        return await user_collection.find_one({"username": username})
--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -5,26 +5,8 @@ from fastapi.security import OAuth2PasswordRequestForm
 from starlette.status import HTTP_401_UNAUTHORIZED
 
 from app.data import User
+from app.data.user_repository import UserRepository
 from app.oauth2_scheme import oauth2_scheme
-
-fake_users_db = {
-    "johndoe": {
-        "id": "1",
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
-        "disabled": False,
-    },
-    "alice": {
-        "id": "2",
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-    },
-}
 
 router = APIRouter(
     tags=["Users"],
@@ -35,46 +17,44 @@ def fake_hash_password(password: str):
     return "fakehashed" + password
 
 
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return User(**user_dict)
+@router.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    response = await UserRepository.get_user_by_username(form_data.username)
+    if response is None:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = User(id=str(response["_id"]), **response)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    return {"access_token": user.username, "token_type": "bearer"}
 
 
-def fake_decode_token(token):
-    user = get_user(fake_users_db, token)
+async def get_user(username: str):
+    return await  UserRepository.get_user_by_username(username)
+
+
+async def fake_decode_token(token):
+    user = await get_user(token)
     return user
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    if not user:
+    user = await fake_decode_token(token)
+    if  user is None:
         raise HTTPException(
             status_code=HTTP_401_UNAUTHORIZED,
             detail="Invalid authentication credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    return user
+    return User(id=str(user["_id"]), **user)
+
 
 async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)],
+        current_user: Annotated[User, Depends(get_current_user)],
 ):
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
-
-
-
-@router.post("/token")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = User(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    return {"access_token": user.username, "token_type": "bearer"}
 
 
 @router.get("/users/me", response_model=User)
