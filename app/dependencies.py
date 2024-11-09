5ead1c9fb25ef8a46a43e048dd05703602665ac3
alphasow <alphaibrahima.sow@outlook.fr>
2024-11-09 12:59:57 +0100

    build(docker): compose

--- app/dependencies.py
+++ app/dependencies.py
@@ -3,5 +3,5 @@ from fastapi.security import OAuth2PasswordBearer
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-client  = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017/")
+client  = motor.motor_asyncio.AsyncIOMotorClient("mongodb://mongo:27017/")
 database = client.get_database("dayder")
